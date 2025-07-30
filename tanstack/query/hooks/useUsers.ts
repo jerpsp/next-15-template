99f5939c224b87a6bab0c31e62d6c07e5920316c
Jeerapong Sanpo <jeerapongsanpo@gmail.com>
2025-07-30 16:08:52 +0700

    add log in tanstack query

--- tanstack/query/hooks/useUsers.ts
+++ tanstack/query/hooks/useUsers.ts
@@ -28,10 +28,11 @@ const fetchUsers = async (
   params: UsersParams
 ): Promise<UsersResponse> => {
   const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT
+  console.log(baseUrl)
   if (!baseUrl) {
     throw new Error("API endpoint is not defined")
   }
-  
+
   if (!token) {
     throw new Error("No access token available")
   }
